d28e3ff4b754aae3bb8d59be4253b3a1f1c26bc4
Wyvian Valença <wgcv2@ic.ufal.br>
2025-07-12 01:54:17 -0300

    feat: class Accounts created to create, store and log in to users accounts

---
+++ source/users/accounts.py
@@ -0,0 +1,41 @@
+from source.users.user import User
+from source.users.adopter import Adopter
+from source.users.shelter import Shelter
+
+class Accounts:
+    def __init__(self):
+        self.adopters_available_id: int = 0
+        self.shelters_available_id: int = 0
+
+        self.users: dict[str, dict[str, Shelter | Adopter]] = {
+            "Adopter": {},
+            "Shelter": {}
+        }
+
+    # LOGIN / SIGNUP
+
+    def create_user(self, type: str, username: str, name: str | None, 
+                    address: str | None, description: str | None
+                    ) -> User | None:
+        if username in self.users[type]:
+            return None
+
+        user: User
+
+        if type == "Adopter":
+            user = Adopter(self.adopters_available_id, username)
+            self.adopters_available_id += 1
+
+        elif type == "Shelter":
+            user = Shelter(self.shelters_available_id, username, name,
+                           address, description)
+            self.shelters_available_id += 1
+
+        self.users[type][user.username] = user
+        return user
+
+    def login(self, type: str, username: str) -> User | None:
+        if username in self.users[type]:
+            return self.users[type][username]
+        else:
+            return None
